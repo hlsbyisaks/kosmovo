3a18fd9f3036414dae42fad5a2c7de84f03c7d3a
smaxigt <maxstromberg1996@gmail.com>
2020-04-13 14:18:26 +0200

    TEXT

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,4 @@
 $(".logout_button").click(function(){
-    userInloged = ""
-    loginPage()
-    WhatPageAreUserOn("login")
     document.cookie = ""
-    clearInterval(updatingInterval)
+    location.reload();
 })
