22f47ac18d0fe3d9341ce3ea178a5bf7da342a21
smaxigt <maxstromberg1996@gmail.com>
2020-04-24 13:04:52 +0200

    TEXT

--- php/questions.php
+++ php/questions.php
@@ -2,12 +2,33 @@
 include('connectToDB.php');
 
 $pdo = connectDB();
-$query = 'SELECT * FROM question';
-$sql = $pdo->prepare($query);
-$sql->execute();
 
-$questions = $sql->fetchAll(\PDO::FETCH_ASSOC);
+if($_GET["activite"] == "getAllQuestion"){
+    $query = 'SELECT *
+        FROM question
+        WHERE NOT EXISTS
+        (SELECT * 
+            FROM userplayed
+            WHERE question.qId = userplayed.qId);';
+    $sql = $pdo->prepare($query);
+    $sql->execute();
 
-$questions = json_encode($questions);
-echo $questions;
+    $questions = $sql->fetchAll(\PDO::FETCH_ASSOC);
+
+    $questions = json_encode($questions);
+    echo $questions;
+
+}else if($_GET["activite"] == "checkifplayed"){
+    $query = 'SELECT * FROM question where qId = ? and play = "no"';
+    $sql = $pdo->prepare($query);
+    $sql->bindParam(1, $_GET['questionID']);
+    $sql->execute();
+
+    $questions = $sql->fetchAll(\PDO::FETCH_ASSOC);
+    if(empty($questions)){
+        echo "NotOK";
+    }else{
+        echo "OK";
+    }
+}
 ?>
