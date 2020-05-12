db4524ee3472251dc349166a5e6dcafffd5f5a3e
hlsbyisaks <56296893+hlsbyisaks@users.noreply.github.com>
2020-05-12 16:32:49 +0200

    isak

--- js/isak.js
+++ js/isak.js
@@ -16,3 +16,34 @@ $("<div>", {
     appendTo:'.logoContainer'
 }).css({'background-image': 'url(img/logo.png)', 'height': '47px', "width": "45px",
         'background-size': 'cover', 'background-repeat': 'no-repeat'}) */
+
+
+
+
+
+//SCOREBOARD
+
+let scoreArr = []
+
+function scoreboard(username) {
+    WhatPageAreUserOn("scoreboard")
+
+    $.get('php/getScore.php', {
+        username: username
+    })
+    .done((data) => {
+        data = JSON.parse(data)
+        console.log(data)
+
+        scoreArr.push([username, data[0]])
+
+
+        scoreArr.forEach(element => {
+            $('<div>', {
+                html: element[0] + ": " + element[1][0],
+                appendTo: '.scoreboard_wrapper'
+            })
+        });
+    })
+    
+}
