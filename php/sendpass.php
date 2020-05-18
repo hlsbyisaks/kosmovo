cf6acf2391529d11be69174e85176933cd0dffbb
smaxigt <maxstromberg1996@gmail.com>
2020-05-18 10:44:12 +0200

    TEXT

--- php/sendpass.php
+++ php/sendpass.php
@@ -1,30 +1,59 @@
 <?php
 include('connectToDB.php');
 
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < 8; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+
+$newPass = randomPassword()
+
+$newPassEncode = password_hash($newPass, PASSWORD_DEFAULT); 
+
 $pdo = connectDB();
-$query = 'SELECT userName, password from user where mail = ?';
+$query = 'UPDATE user SET user.password = ? where user.mail = ?';
 $sql = $pdo->prepare($query);
-$sql->bindParam(1, $_GET['mail']);
+$sql->bindParam(1, $newPassEncode);
+$sql->bindParam(2, $_GET['mail']);
 $sql->execute();
 
-$status = $sql->fetchAll(\PDO::FETCH_ASSOC);
 
-$status = json_encode($status);
+$to = $_GET["mail"];
+$subject = "Kosmovo - Pasword Reset (No-Reply)";
 
-$to = $_GET['mail'];
-$subject = "Username & Password";
-         
-$message = "<b>".$status."</b>";
-$message .= "<h1>Kosmovo - Username & Password</h1>";
-         
-$header = "From:kosmovo@gmail.com \r\n";
-         
-$retval = mail ($to,$subject,$message,$header);
-         
-if( $retval == true ) {
-    echo "Message sent successfully...";
-}else {
-    echo "Message could not be sent...";
-}
+$message = "
+<html>
+<head>
+<title>Kosmovo</title>
+</head>
+<body>
+<p>Here is your new password</p>
+<table>
+<tr>
+<th>".$newPass."</th>
+</tr>
+</table>
+<
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <kosmovo@no-reply.com>' . "\r\n";
+$headers .= 'Cc: kosmovo@no-reply.com' . "\r\n";
+
+mail($to,$subject,$message,$headers);
+
 
 ?>
